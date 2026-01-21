fd3737876841be739a6b369d6dd5d7ca34b557f7
suha <suha@DESKTOP-K60DNE8>
2026-01-21 11:29:20 +0000

    Scaffolding Dbcontext done

---
+++ BooksWebAPI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksWebAPI.Models;
+
+public partial class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int DisplayName { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ BooksWebAPI/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksWebAPI.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Isbn { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public double ListPrice { get; set; }
+
+    public double Price { get; set; }
+
+    public double Price50 { get; set; }
+
+    public double Price100 { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+
+    public virtual Category Category { get; set; } = null!;
+}

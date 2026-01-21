e70f767f6d44e2c3a19d704e0f25136e57f183b8
suha <suha@DESKTOP-K60DNE8>
2026-01-21 11:33:20 +0000

    Added API controller with read and write

---
+++ BooksWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using BooksWebAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksWebAPI.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db=db;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

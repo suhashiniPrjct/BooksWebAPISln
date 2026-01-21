fe9c3b3ee6585ea6f57938eb7a4bff948e7e8b3c
suha <suha@DESKTOP-K60DNE8>
2026-01-21 11:44:07 +0000

    feat:Service architecture is implemented

---
+++ BooksWebAPI/Application/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using BooksWebAPI.Application.DTOs;
+
+namespace BooksWebAPI.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductReadDTO>> GetallAsyn();
+        Task<ProductReadDTO> GetByIdAsync(int id);
+        Task<ProductReadDTO> CreateAsync(ProductCreateDTO productCreateDTO);
+        Task<bool> UpdateAsync(int id,ProductCreateDTO productCreateDTO);
+        Task<bool> DeleteAsync(int id);
+    }
+}

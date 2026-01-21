4569764666ee74a245f0da99a90cc5846cddf08e
suha <suha@DESKTOP-K60DNE8>
2026-01-21 13:05:13 +0000

    DTOs created

---
+++ BooksWebAPI/Application/DTOs/ProductReadDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace BooksWebAPI.Application.DTOs
+{
+    public class ProductReadDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Isbn { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public double ListPrice { get; set; }
+
+        public double Price { get; set; }
+
+        public double Price50 { get; set; }
+
+        public double Price100 { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}

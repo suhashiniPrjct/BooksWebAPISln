22e39f2fbf2d10703d5845d6ed1a481bd48c8180
suha <suha@DESKTOP-K60DNE8>
2026-01-21 18:33:16 +0000

    feat:DTOs are created for entities

---
+++ BooksWebAPI/Application/DTOs/ProductCreateDTO.cs
@@ -0,0 +1,25 @@
+﻿using BooksWebAPI.Domain.Entities;
+
+namespace BooksWebAPI.Application.DTOs
+{
+    public class ProductCreateDTO
+    {
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Isbn { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public double ListPrice { get; set; }
+
+        public double Price { get; set; }
+
+        public double Price50 { get; set; }
+
+        public double Price100 { get; set; }
+        public string ImageUrl { get; set; } = null!;
+
+    }
+}

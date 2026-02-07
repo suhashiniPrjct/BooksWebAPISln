9a695f1fb93c8955165763fdb6db2aef71d0d024
suha <suha@DESKTOP-K60DNE8>
2026-02-07 18:47:15 +0000

    Defect fix: nable to resolve service for type Ilogger

---
+++ BooksWebAPI/API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace BooksWebAPI.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            //switch expression based the exception assigning the statuscode
+            var statusCode = exception switch
+            {
+                KeyNotFoundException => HttpStatusCode.NotFound,
+                UnauthorizedAccessException => HttpStatusCode.Unauthorized,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            var response = new
+            {
+                statusCode = (int)statusCode,
+                message = exception.Message
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}

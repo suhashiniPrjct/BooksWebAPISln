e504b1d4403f59b45ad1fa66b16adfb8b80e2219
suha <suha@DESKTOP-K60DNE8>
2026-02-15 21:30:35 +0000

    Enhancement:Removed Inline tryCatch.Error handling in Middleware

--- BooksWebAPI/API/Controllers/ProductController.cs
+++ BooksWebAPI/API/Controllers/ProductController.cs
@@ -16,7 +16,6 @@ namespace BooksWebAPI.API.Controllers
     public class ProductController : ControllerBase
     {
 
-        //private readonly AppDbContext _context;
         private readonly IProductService _pservice;
         private readonly ILogger<ProductController> _logger;
         public ProductController(IProductService pservice, ILogger<ProductController> logger)
@@ -25,58 +24,105 @@ namespace BooksWebAPI.API.Controllers
             _logger = logger;
         }
 
-        // GET: api/<ProductController>
+        // GET: api/Product
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            _logger.LogInformation("Get all products");
-            return Ok(await _pservice.GetallAsyn());
+            //try
+            //{
+                _logger.LogInformation("Getting all products");
+                var products = await _pservice.GetallAsync();
+                return Ok(products);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "Unexpected error fetching all products");
+            //    return BadRequest(ex);
+            //}
+
+            
         }
 
-        // GET api/<ProductController>/5
+        // GET api/Product/5
         [HttpGet("{id}")]
-        [Authorize(Roles = "Employee")]
-        public async Task<IActionResult> GetById(int id)
+        [Authorize(Roles = "Employee,Admin")]
+        public async Task<IActionResult?> GetById(int id)
         {
-            try
-            {
+            //Unhandled exceptions handled at Custom Middleware
+            //try
+            //{
+
+                _logger.LogInformation("Fetching product with id: {id}", id);                               
                 var product = await _pservice.GetByIdAsync(id);
+                if(product == null)
+                {
+                    _logger.LogWarning("Product not found for the id {id}",id);
+                    return NotFound("Product not found");
+                }
+                else
+                {
+                    return Ok(product);
+                }                  
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "Unexpected error  while fetching portfolio for clientId {ClientId}", id);
 
-                return product == null ? NotFound() : Ok(product);
-
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error while fetching portfolio for clientId {ClientId}", id);
-
-                return StatusCode(500,"An error occured");
-            }
+            //    return StatusCode(500,"An error occured");
+            //}
 
         }
 
-        // POST api/<ProductController>
+        // POST api/Product
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] ProductCreateDTO Cdto)
+        [Authorize("Admin")]
+        public async Task<IActionResult> Create([FromBody] ProductCreateDTO PCreateDTO)
         {
-            var newProdDTO= await _pservice.CreateAsync(Cdto);
-            return CreatedAtAction(nameof(GetById),new {id=newProdDTO.Id},newProdDTO);
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("Invalid Product creation");
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                var newProdDTO = await _pservice.CreateAsync(PCreateDTO);
+                _logger.LogInformation("new product is created with id: {id}", newProdDTO.Id);
+                return CreatedAtAction(nameof(GetById), new { id = newProdDTO.Id }, newProdDTO);
+            }
         }
 
-        // PUT api/<ProductController>/5
+        // PUT api/Product/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] ProductUpdateDTO Updatdto)
+        public async Task<IActionResult> Update(int id, [FromBody] ProductUpdateDTO PUpdateDTO)
         {
-            var Updatedprod=await _pservice.UpdateAsync(id,Updatdto);
-            return Updatedprod ? NoContent() : NotFound();
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Unsuccesful attempt of updating the product id: {id}", id);
+                return BadRequest(ModelState);
+            }
+            var Updatedprod = await _pservice.UpdateAsync(id, PUpdateDTO);
+            if(!Updatedprod)
+            {
+                _logger.LogWarning("Product not found for update with id: {Id}", id);
+                return NotFound("Product not found");
+            }
+            return NoContent();
         }
 
         // DELETE api/<ProductController>/5
         [HttpDelete("{id}")]
-        
+        [Authorize("Admin")]
         public async Task<IActionResult> Delete(int id)
         {
+
             var deleted=await _pservice.DeleteAsync(id);
-            return deleted ? NoContent() : NotFound();
+            if (!deleted)
+            {
+                _logger.LogWarning("Product not found for deletion with id: {Id}", id);
+                return NotFound("Product not found"); 
+            }
+            _logger.LogWarning("Deleted the product with id: {Id}", id);
+            return NoContent();
         }
     }
 }
